33f4ef4c221881c524513782b3ba5bbf3cea39ba
Saikat <saikat212saikat@gmail.com>
2023-03-10 03:37:47 +0600

    User Registration Model Created

---
+++ Inventory Management System/Models/Registration.cs
@@ -0,0 +1,28 @@
+﻿using Humanizer;
+using static Humanizer.In;
+using static Inventory_Management_System.Models.Registration;
+using System.Net;
+
+namespace Inventory_Management_System.Models
+{
+    public class Registration
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+
+        public UserPreferences Preferences { get; set; }
+     
+        public class UserPreferences
+        {
+            public string FavoriteColor { get; set; }
+            public string FavoriteFood { get; set; }
+        }
+
+
+    }
+}
