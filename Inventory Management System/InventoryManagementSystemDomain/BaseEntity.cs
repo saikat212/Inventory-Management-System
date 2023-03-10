67ff7cb0483ef85e4ae3e110cfb4d06fbbd84108
Saikat <saikat212saikat@gmail.com>
2023-03-10 08:15:38 +0600

    Error fixing at InventoryManagementSystemInfrastructure

--- Inventory Management System/InventoryManagementSystemDomain/BaseEntity.cs
+++ Inventory Management System/InventoryManagementSystemDomain/BaseEntity.cs
@@ -1,6 +1,27 @@
-﻿namespace Inventory_Management_System.InventoryManagementSystemDomain
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_Management_System.InventoryManagementSystemDomain
 {
     public class BaseEntity
     {
+        public BaseEntity()
+        {
+            CreatedDate = DateTime.Now;
+        }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public int UpdatedBy { get; set; }
+    }
+
+    public class SelectOption
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
     }
 }
